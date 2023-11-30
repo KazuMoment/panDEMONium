4d8b30bd7d39ae50dec4aa635395c497fb412daf
JonBabao <130745282+JonBabao@users.noreply.github.com>
2023-11-30 11:38:05 +0800

    Update JDBC.java

--- src/main/JDBC.java
+++ src/main/JDBC.java
@@ -1,87 +1,97 @@
 package main;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBC {
-    String url = "jdbc:mysql://localhost:3306/panDEMONium";
-    String username = "root";
-    String password = "";
+    String url = "jdbc:sqlite:mydatabase.db";
+    public Connection connection = null;
+    public Statement statement = null;
     public int SessionID;
     
-    public void toDatabase(String query){
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    public void createDatabase() throws ClassNotFoundException {
+        // JDBC URL for SQLite (jdbc:sqlite:filename.db)
+        String url = "jdbc:sqlite:mydatabase.db";
 
-            Connection connection = DriverManager.getConnection(url, username, password);
-            
-            Statement statement = connection.createStatement();
+        // JDBC variables for opening, closing, and managing connection
+        
 
-            statement.execute(query);
-           /* 
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
-            }*/
-        }
-        catch(Exception e){
-            System.out.println(e);
+        try {
+            // Register JDBC driver (not needed for SQLite)
+            Class.forName("org.sqlite.JDBC");
+
+            // Open a connection to the SQLite database (creates the file if it doesn't exist)
+            System.out.println("Connecting to database...");
+            connection = DriverManager.getConnection(url);
+
+            // Execute a query to create a table
+            System.out.println("Creating table...");
+            statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS playerrecords (SessionID INT(5) NOT NULL , Level INT(5) NULL , MaxHP"+
+                        " INT(5) NULL , HP INT(5) NULL , MaxMana INT(5) NULL , Mana INT(5) NULL , Strength"+
+                        " INT(5) NULL , Dexterity INT(5) NULL , Gold INT(10) NULL , Weapon VARCHAR(20) NULL ,"+ 
+                        " Shield VARCHAR(20) NULL , Playtime TIME NULL , PRIMARY KEY (SessionID))";
+            statement.executeUpdate(sql);
+            System.out.println("Table created successfully");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close resources
+            try {
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
-    public int getLastSessionID(){
-        String url = "jdbc:mysql://localhost:3306/panDEMONium";
-        String username = "root";
-        String password = "";
-        int i;
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url, username, password);
-            
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select max(SessionID) from playerdata");
-
+    public void toDatabase(String query) {
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement()) {
+            Class.forName("org.sqlite.JDBC");
+            statement.executeUpdate(query);
+            // You can handle the result set or any other processing here if needed
+        } catch (Exception e) {
+            e.printStackTrace(); // Log or handle the exception appropriately
+        }
+    }
+    
+    public int getLastSessionID() {
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT MAX(SessionID) FROM playerrecords")) {
+            Class.forName("org.sqlite.JDBC");
+    
             if (resultSet.next()) {
-                i = resultSet.getInt(1);
-                return i;
-            }
-            else{
+                return resultSet.getInt(1);
+            } else {
                 return -1;
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
             return -1;
         }
     }
-    public String getValue(int columnIndex, int sessionID){
-        String url = "jdbc:mysql://localhost:3306/panDEMONium";
-        String username = "root";
-        String password = "";
-        String i;
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url, username, password);
-            
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from playerdata where sessionID = " + sessionID);
-
+    
+    public String getValue(int columnIndex, int sessionID) {
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM playerrecords WHERE sessionID = " + sessionID)) {
+            Class.forName("org.sqlite.JDBC");
+    
             if (resultSet.next()) {
-                i = resultSet.getString(columnIndex);
-                return i;
-            }
-            else{
+                return resultSet.getString(columnIndex);
+            } else {
                 return "null";
             }
-        }
-        catch(Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            e.printStackTrace();
             return "null";
         }
     }
+    
 }
